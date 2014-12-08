f0465700c75d9dbaba5df7254cbacf04f9eeda85
valdrinkoshi <valdrinkoshi@gmail.com>
2014-12-07 16:45:41 -0800

    use Parse in the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,5 @@
 'use strict';
 var Parse = require('parse').Parse;
-Parse.initialize('LaxhuAA5aa4vMQ7SfNfDchfXgMETj2xVl3tGoMWC', 'ajYR5N3u2d1G4f5bZ52HBJwFCZRyJ4S3CNLAk5Il');
 
 // Ionic Starter App
 
@@ -9,77 +8,77 @@ Parse.initialize('LaxhuAA5aa4vMQ7SfNfDchfXgMETj2xVl3tGoMWC', 'ajYR5N3u2d1G4f5bZ5
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('HomeCooked', ['ionic', 'HomeCooked.controllers'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('app', {
-      url: '/app',
-      abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl'
-    })
-
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/search.html'
-        }
+  .constant('Parse', Parse)
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory b ar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    })
-
-    .state('app.buyer', {
-      url: '/buyer',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/buyer.html',
-          controller: 'BuyerCtrl'
-        }
-      }
-    })
-    .state('app.seller', {
-      url: '/seller',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/seller.html',
-          controller: 'SellerCtrl'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.chefs', {
-      url: '/buyer/:zipcode',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlist.html',
-          controller: 'PlaylistCtrl'
-        }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/buyer');
-});
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.buyer', {
+        url: '/buyer',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/buyer.html',
+            controller: 'BuyerCtrl'
+          }
+        }
+      })
+      .state('app.seller', {
+        url: '/seller',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/seller.html',
+            controller: 'SellerCtrl'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+
+      .state('app.chefs', {
+        url: '/buyer/:zipcode',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/buyer');
+  });
 
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,64 +1,67 @@
 'use strict';
 angular.module('HomeCooked.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-    //$scope.login();
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Open the login modal
-  $scope.logout = function() {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, Parse) {
+    // Form data for the login modal
+    Parse.initialize('LaxhuAA5aa4vMQ7SfNfDchfXgMETj2xVl3tGoMWC', 'ajYR5N3u2d1G4f5bZ52HBJwFCZRyJ4S3CNLAk5Il');
     $scope.loginData = {};
-    $scope.modal.show();
-  };
+    $scope.currentUser = Parse.User.current();
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+      if ($scope.currentUser) {
+        $scope.login();
+      }
+    });
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-.controller('PlaylistCtrl', function() {
-})
-.controller('BuyerCtrl', function($scope) {
-  $scope.findChefs = function(){
-    console.log(arguments);
-  };
-  $scope.openOrders = function(){
-    console.log(arguments);
-  };
-});
+    // Open the login modal
+    $scope.logout = function () {
+      $scope.loginData = {};
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      {title: 'Reggae', id: 1},
+      {title: 'Chill', id: 2},
+      {title: 'Dubstep', id: 3},
+      {title: 'Indie', id: 4},
+      {title: 'Rap', id: 5},
+      {title: 'Cowbell', id: 6}
+    ];
+  })
+
+  .controller('PlaylistCtrl', function () {})
+  .controller('BuyerCtrl', function ($scope) {
+    $scope.findChefs = function () {
+      console.log(arguments);
+    };
+    $scope.openOrders = function () {
+      console.log(arguments);
+    };
+  });
