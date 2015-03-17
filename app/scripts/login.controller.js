0cea919faa481b172e6febe9da185c9a9996215a
valdrinkoshi <valdrinkoshi@gmail.com>
2015-03-17 00:01:16 -0700

    check if seller view already

--- app/scripts/login.controller.js
+++ app/scripts/login.controller.js
@@ -1,59 +1,62 @@
 'use strict';
-angular.module('HomeCooked.controllers').controller('LoginCtrl', ['$scope', '$ionicModal', '$ionicLoading', '$state', '$ionicPopup', 'LoginService',
-    function ($scope, $ionicModal, $ionicLoading, $state, $ionicPopup, LoginService) {
-      $scope.doingLogin = false;
-      $scope.doingSignup = false;
-      $ionicLoading.show({
-        template: 'Checking login...'
-      });
-      // Create the login modal that we will use later
-      $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope,
-        backdropClickToClose: false,
-        hardwareBackButtonClose: false
-      }).then(function (modal) {
-        $scope.modal = modal;
-        LoginService.getLoginStatus().then(function gotLoginStatus(userInfo) {
-          $ionicLoading.hide();
-          if (userInfo) {
-            $scope.user = userInfo;
-          }
-          else {
-            modal.show();
-          }
-        }, function didntGotLoginStatus() {
-          $ionicLoading.hide();
-          modal.show();
-        });
-      });
-
-      $scope.logout = function () {
-        LoginService.logout();
-        $scope.modal.show();
-      };
-      $scope.isSellerView = false;
-      $scope.switchView = function () {
-        $scope.isSellerView = !$scope.isSellerView;
-        //TODO check if he can be seller
-        $state.go($scope.isSellerView ? 'app.seller' : 'app.buyer');
-      };
-
-      // Perform the login action when the user submits the login form
-      $scope.login = function (loginType, user, pass) {
-        $ionicLoading.show({
-          template: 'Doing login...'
-        });
-        LoginService.login(loginType, user, pass).then(function didLogin(userInfo) {
+angular.module('HomeCooked.controllers').controller('LoginCtrl', ['$scope', '$ionicModal', '$ionicLoading', '$state', '$ionicPopup', '$timeout', '$ionicSideMenuDelegate', 'LoginService',
+  function ($scope, $ionicModal, $ionicLoading, $state, $ionicPopup, $timeout, $ionicSideMenuDelegate, LoginService) {
+    $scope.doingLogin = false;
+    $scope.doingSignup = false;
+    $ionicLoading.show({
+      template: 'Checking login...'
+    });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope,
+      backdropClickToClose: false,
+      hardwareBackButtonClose: false
+    }).then(function (modal) {
+      $scope.modal = modal;
+      LoginService.getLoginStatus().then(function gotLoginStatus(userInfo) {
+        $ionicLoading.hide();
+        if (userInfo) {
           $scope.user = userInfo;
-          $ionicLoading.hide();
-          $scope.modal.hide();
-          $scope.doingLogin = $scope.doingSignup = false;
-        }, function didNotLogin(err) {
-          $ionicLoading.hide();
-          $ionicPopup.alert({
-            title: 'Couldn\'t login',
-            template: err
-          });
+        }
+        else {
+          modal.show();
+        }
+      }, function didntGotLoginStatus() {
+        $ionicLoading.hide();
+        modal.show();
+      });
+    });
+
+    $scope.logout = function () {
+      LoginService.logout();
+      $scope.modal.show();
+    };
+    $scope.isSellerView = $state.current.name === 'app.seller';
+    $scope.switchView = function () {
+      $scope.isSellerView = !$scope.isSellerView;
+      //TODO check if he can be seller
+      $timeout(function () {
+        $ionicSideMenuDelegate.toggleLeft(false);
+        $state.go($scope.isSellerView ? 'app.seller' : 'app.buyer');
+      }, 250);
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.login = function (loginType, user, pass) {
+      $ionicLoading.show({
+        template: 'Doing login...'
+      });
+      LoginService.login(loginType, user, pass).then(function didLogin(userInfo) {
+        $scope.user = userInfo;
+        $ionicLoading.hide();
+        $scope.modal.hide();
+        $scope.doingLogin = $scope.doingSignup = false;
+      }, function didNotLogin(err) {
+        $ionicLoading.hide();
+        $ionicPopup.alert({
+          title: 'Couldn\'t login',
+          template: err
         });
-      };
-    }]);
+      });
+    };
+  }]);
