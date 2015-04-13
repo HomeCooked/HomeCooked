2bebf91a5d8209cbcde3392fab89ca2eea08af39
valdrinkoshi <valdrinkoshi@gmail.com>
2015-04-12 19:41:27 -0700

    add main controller & view. Load only once the controllers from views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,34 +6,43 @@ angular.module('HomeCooked.controllers', ['HomeCooked.services']);
 angular.module('HomeCooked.services', []);
 
 myApp.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory b ar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-  })
-  .constant("BASE_URL", "//homecooked.herokuapp.com")
-  .constant("CLIENT_ID", "111")
-  .constant("CACHE_ID", "homecooked")
+  $ionicPlatform.ready(function () {
+    // Hide the accessory b ar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+  .constant('BASE_URL', '//homecooked.herokuapp.com')
+  .constant('CLIENT_ID', '111')
+  .constant('CACHE_ID', 'homecooked')
   .config(function ($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
   })
   .config(function ($stateProvider, $urlRouterProvider) {
+
     window.openFB.init({appId: '805673482820123'});
+
     $stateProvider
       .state('app', {
         url: '/app',
         abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'LoginCtrl'
+        templateUrl: 'templates/menu.html'
       })
 
+      .state('app.main', {
+        url: '/main',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/main.html'
+          }
+        }
+      })
       .state('app.buyer', {
         url: '/buyer',
         views: {
@@ -46,11 +55,10 @@ myApp.run(function ($ionicPlatform) {
         url: '/seller',
         views: {
           'menuContent': {
-            templateUrl: 'templates/chef.html',
-            controller: 'ChefCtrl'
+            templateUrl: 'templates/chef.html'
           }
         }
       });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/buyer');
+    $urlRouterProvider.otherwise('/app/main');
   });
