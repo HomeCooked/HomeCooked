ec63bf2cc74393710a21696453732b971eb22129
valdrinkoshi <valdrinkoshi@gmail.com>
2015-04-19 18:55:43 -0700

    enable buyer/seller mode from main

--- app/scripts/main.controller.js
+++ app/scripts/main.controller.js
@@ -1,5 +1,13 @@
 'use strict';
-angular.module('HomeCooked.controllers').controller('MainCtrl', [
-  function () {
+angular.module('HomeCooked.controllers').controller('MainCtrl', ['$state', 'LoginService',
+  function ($state, LoginService) {
 
+    this.buyerMode = function () {
+      LoginService.getUser().isEnrolled = false;
+      $state.go('app.buyer');
+    };
+    this.sellerMode = function () {
+      LoginService.getUser().isEnrolled = true;
+      $state.go('app.seller');
+    };
   }]);
