d246082ba819301ef15f7218b7ddb0cf09cad72f
dbaq <didier.baquier@myerp.com>
2015-06-04 16:36:03 -0700

    refactor settings controller
    - wrap in an IIFE
    - improve DI readability

--- app/scripts/settings.controller.js
+++ app/scripts/settings.controller.js
@@ -1,9 +1,19 @@
-'use strict';
-angular.module('HomeCooked.controllers').controller('SettingsCtrl', ['$scope', 'LoginService',
-  function($scope, LoginService) {
-    var that = this;
-    //check if user changes, to show or not settings
-    $scope.$on('$ionicView.beforeEnter', function() {
-      that.user = LoginService.getUser();
-    });
-  }]);
+(function() {
+    'use strict';
+    
+    angular
+        .module('HomeCooked.controllers')
+        .controller('SettingsCtrl', SettingsCtrl);
+
+    SettingsCtrl.$inject = ['$scope', 'LoginService'];
+    
+    function SettingsCtrl($scope, LoginService) {
+
+        var vm = this;
+
+        $scope.$on('$ionicView.beforeEnter', function() {
+          vm.user = LoginService.getUser();
+        });
+    }
+
+})();
