8f71a3532e2a50c1cc7b7f6c7f66dfd1ec272287
dbaq <didier.baquier@myerp.com>
2015-06-05 14:44:27 -0700

    email: detect changes and save user email if needed
    API call needs to be done

--- app/scripts/settings.controller.js
+++ app/scripts/settings.controller.js
@@ -5,15 +5,44 @@
         .module('HomeCooked.controllers')
         .controller('SettingsCtrl', SettingsCtrl);
 
-    SettingsCtrl.$inject = ['$scope', 'LoginService'];
+    SettingsCtrl.$inject = ['$rootScope', '$scope', '$state', '$timeout', '$ionicLoading', 'LoginService'];
     
-    function SettingsCtrl($scope, LoginService) {
+    function SettingsCtrl($rootScope, $scope, $state, $timeout, $ionicLoading, LoginService) {
 
         var vm = this;
+        vm.onChange = onChange;
 
         $scope.$on('$ionicView.beforeEnter', function() {
           vm.user = LoginService.getUser();
         });
+
+
+        var deregisterStateChangeStart = $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
+            // detect the navigation between a setting page to the main setting page
+            if (toState && fromState && toState.name === 'app.settings' &&  fromState.name.indexOf('app.settings') > -1) {
+                updateUserProperties(event, toState.name);
+            }
+            deregisterStateChangeStart();
+        });
+
+        function updateUserProperties(event, toStateName) {
+            if (vm.userPropertiesChanged) {
+                event.preventDefault();
+                $ionicLoading.show({
+                    template: 'Saving...'
+                });
+                $timeout(function() {
+                    //API CALL
+                    console.log(vm.user);
+                    $ionicLoading.hide();
+                    $state.go(toStateName);
+                }, 1500);
+            }
+        }
+
+        function onChange() {
+            vm.userPropertiesChanged = true;
+        }
     }
 
 })();
