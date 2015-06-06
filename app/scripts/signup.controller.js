7649d7aa14005b3d13d35473bc7025693eda88c4
dbaq <didier.baquier@myerp.com>
2015-06-06 11:17:34 -0700

    add sign up page style
    - add also mixins.scss, a bunch of useful mixins that can be reused across all the scss files

--- app/scripts/signup.controller.js
+++ app/scripts/signup.controller.js
@@ -6,10 +6,22 @@ angular
     .module('HomeCooked.controllers')
     .controller('SignupCtrl', SignupCtrl);
 
-    SignupCtrl.$inject = ['$scope', 'LoginService'];
+    SignupCtrl.$inject = ['$scope', '$timeout', '$ionicSideMenuDelegate', '$ionicNavBarDelegate', 'LoginService'];
 
-    function SignupCtrl($scope, LoginService) {
+    function SignupCtrl($scope, $timeout, $ionicSideMenuDelegate, $ionicNavBarDelegate, LoginService) {
         var vm = this;
+
+
+        activate();
+
+        function activate() {
+            $timeout(function() {
+                //wrapping the delegates in a timeout is required to avoid a race condition
+                //otherwise the nav bar remains displayed
+                $ionicSideMenuDelegate.canDragContent(false);
+                $ionicNavBarDelegate.showBar(false);
+            });
+        }
     }
 
 })();
