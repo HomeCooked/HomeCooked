25bfc953fde2cfbadff4aff704132ee4fb19132b
dbaq <didier.baquier@myerp.com>
2015-06-09 19:58:42 -0700

    fake validation of the zip code

--- app/scripts/zipcoderestriction.controller.js
+++ app/scripts/zipcoderestriction.controller.js
@@ -5,11 +5,12 @@
         .module('HomeCooked.controllers')
         .controller('ZipCodeRestrictionCtrl', ZipCodeRestrictionCtrl);
 
-    ZipCodeRestrictionCtrl.$inject = [];
+    ZipCodeRestrictionCtrl.$inject = ['$timeout', '$ionicHistory', '$state', '$ionicLoading'];
 
-    function ZipCodeRestrictionCtrl() {
+    function ZipCodeRestrictionCtrl($timeout, $ionicHistory, $state, $ionicLoading) {
 
         var vm = this;
+        vm.validZipCode = validZipCode;
 
         vm.map = {
             defaults: {
@@ -38,6 +39,31 @@
                 zoom: 14
             }
         };
+
+        activate();
+
+
+        function activate() {
+
+        }
+
+        function validZipCode() {
+            if (vm.zipcode && parseInt(vm.zipcode) > 0) {
+                $ionicLoading.show({
+                    template: 'Checking...'
+                });
+                //FAKE API CALL
+                $timeout(function() {
+                    $ionicLoading.hide();
+                    if (true) {
+                        $ionicHistory.nextViewOptions({
+                          historyRoot: true
+                        });
+                        $state.go('app.buyer');
+                    }
+                }, 1000);
+            }
+        }
 
     }
 
