c550041534edd5de62237221ff41c4c0c40f8b4f
valdrinkoshi <valdrinkoshi@gmail.com>
2015-06-17 00:38:09 -0700

    added payment service

--- app/scripts/payment.controller.js
+++ app/scripts/payment.controller.js
@@ -5,21 +5,29 @@
     .module('HomeCooked.controllers')
     .controller('PaymentCtrl', PaymentCtrl);
 
-  PaymentCtrl.$inject = ['$scope', 'HCMessaging'];
+  PaymentCtrl.$inject = ['$scope', '$ionicLoading', 'HCMessaging', 'PaymentService'];
 
-  function PaymentCtrl($scope, HCMessaging) {
+  function PaymentCtrl($scope, $ionicLoading, HCMessaging, PaymentService) {
 
-    var vm = this;
-    vm.stripeCallback = stripeCallback;
+    $scope.stripeCallback = stripeCallback;
+    $scope.showLoading = showLoading;
 
     $scope.$on('$ionicView.beforeEnter', function () {
       // TODO get values from server!
-      vm.number = vm.expiry = vm.cvc = null;
+      $scope.number = $scope.expiry = $scope.cvc = undefined;
     });
+
+    function showLoading() {
+      $ionicLoading.show({template: 'Saving...'});
+    }
 
     function stripeCallback(code, result) {
       if (result.error) {
+        $ionicLoading.hide();
         HCMessaging.showError(result.error);
+      }
+      else {
+        PaymentService.savePaymentInfo(result).catch(HCMessaging.showError).finally($ionicLoading.hide);
       }
     }
   }
