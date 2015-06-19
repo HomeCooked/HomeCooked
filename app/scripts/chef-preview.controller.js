b3780e27b411514a73d1fcba17f431b12f1dce69
dbaq <didier.baquier@myerp.com>
2015-06-18 23:56:36 -0700

    chef and dish list preview page

--- app/scripts/chef-preview.controller.js
+++ app/scripts/chef-preview.controller.js
@@ -6,10 +6,12 @@
         .module('HomeCooked.controllers')
         .controller('ChefPreviewCtrl', ChefPreviewCtrl);
 
-    ChefPreviewCtrl.$inject = ['$stateParams'];
+    ChefPreviewCtrl.$inject = ['$stateParams', '$ionicHistory'];
 
-    function ChefPreviewCtrl($stateParams) {
+    function ChefPreviewCtrl($stateParams, $ionicHistory) {
         var vm = this;
+
+        vm.goToPrevious = $ionicHistory.goBack;
 
         vm.chef = {};
 
@@ -29,7 +31,37 @@
                     latitude: 37.7551522,
                     longitude: -122.4260917
                 },
-                dishes: []
+                bio: 'Growing up in a Greek and Sicilian family, the Cleveland native creates boldly flavored, ' + 
+                    'deeply satisfying dishes at his four restaurants in America’s heartland.',
+                dishes: [{
+                    title: 'Philly Cheese Steak',
+                    picture: 'http://i.bullfax.com/imgs/962fd564649084eabfe59808c745c2220a23883c.jpg',
+                    price: '$7.99',
+                    available_qty: 3,
+                    pickup_time: '7pm',
+                    review_count: 22
+                }, {
+                    title: 'Philly Cheese Steak',
+                    picture: 'http://cdn.crownmediadev.com/d1/720d567fca26a5b363ecd6d6b74976/calamari-segment-Ep060.jpg',
+                    price: '$5.99',
+                    available_qty: 2,
+                    pickup_time: '8pm',
+                    review_count: 22
+                }, {
+                    title: 'Philly Cheese Steak',
+                    picture: 'http://www.muscleandfitness.com/sites/muscleandfitness.com/files/philly-cheesesteak-recipe_0.jpg',
+                    price: '$5.99',
+                    available_qty: 3,
+                    pickup_time: '7pm',
+                    review_count: 22
+                }, {
+                    title: 'Philly Cheese Steak',
+                    picture: 'http://www.muscleandfitness.com/sites/muscleandfitness.com/files/philly-cheesesteak-recipe_0.jpg',
+                    price: '$5.99',
+                    available_qty: 3,
+                    pickup_time: '7pm',
+                    review_count: 22
+                }]
             };
         }
 
