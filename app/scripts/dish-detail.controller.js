736b6b345b4d47e4412a16de45f1eac38dd654d8
dbaq <didier.baquier@myerp.com>
2015-06-24 22:54:58 -0700

    display more informations

--- app/scripts/dish-detail.controller.js
+++ app/scripts/dish-detail.controller.js
@@ -18,12 +18,16 @@
         activate();
 
         function activate() {
-
+            vm.chef = {
+                distance: '0.2mi',
+                cross_street: 'Guerrero st & 21st st'
+            };
             vm.dish = {
                 title: 'Philly Cheese Steak',
                 picture: 'http://www.muscleandfitness.com/sites/muscleandfitness.com/files/philly-cheesesteak-recipe_0.jpg',
                 price: '$7.99',
                 available_qty: 3,
+                review_count: 22,
                 pickup_time: '7pm',
                 id: $stateParams.dishId,
                 ingredients: ['pizza', 'mushrooms', 'onions','cheese', 'more cheese']
