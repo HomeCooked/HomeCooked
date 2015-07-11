ca64c0b81406ec4fde02e5b91de45bae32049f99
valdrinkoshi <valdrinkoshi@gmail.com>
2015-07-10 20:13:51 -0700

    4 space guy

--- app/scripts/zipcoderestriction.controller.js
+++ app/scripts/zipcoderestriction.controller.js
@@ -1,123 +1,123 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('HomeCooked.controllers')
-    .controller('ZipCodeRestrictionCtrl', ZipCodeRestrictionCtrl);
+    angular
+        .module('HomeCooked.controllers')
+        .controller('ZipCodeRestrictionCtrl', ZipCodeRestrictionCtrl);
 
-  ZipCodeRestrictionCtrl.$inject = ['$stateParams', '$timeout', '$ionicHistory', '$state', '$ionicLoading', '$ionicPopup',
-    'LoginService', 'ZipcodesService'];
+    ZipCodeRestrictionCtrl.$inject = ['$stateParams', '$timeout', '$ionicHistory', '$state', '$ionicLoading', '$ionicPopup',
+        'LoginService', 'ZipcodesService'];
 
-  function ZipCodeRestrictionCtrl($stateParams, $timeout, $ionicHistory, $state, $ionicLoading, $ionicPopup, LoginService, ZipcodesService) {
+    function ZipCodeRestrictionCtrl($stateParams, $timeout, $ionicHistory, $state, $ionicLoading, $ionicPopup, LoginService, ZipcodesService) {
 
-    var vm = this;
-    vm.validZipCode = validZipCode;
-    vm.registerEmail = registerEmail;
+        var vm = this;
+        vm.validZipCode = validZipCode;
+        vm.registerEmail = registerEmail;
 
-    activate();
+        activate();
 
-    function activate() {
-      if ($state.current.name === 'zipcode-validation') {
-        initMapProperties();
-        $timeout(function() {
-          navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
-        }, 500);
-      }
-    }
-
-    function onLocationSuccess(position) {
-      var coords = position.coords;
-      vm.map.center = {
-        lat: coords.latitude,
-        lng: coords.longitude,
-        zoom: 14
-      };
-      vm.map.markers = {
-        marker: {
-          lat: coords.latitude,
-          lng: coords.longitude
+        function activate() {
+            if ($state.current.name === 'zipcode-validation') {
+                initMapProperties();
+                $timeout(function() {
+                    navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
+                }, 500);
+            }
         }
-      };
-    }
 
-    function onLocationError() {
-      $ionicPopup.alert({
-        title: 'Error',
-        template: 'Unable to retrieve your location'
-      });
-    }
+        function onLocationSuccess(position) {
+            var coords = position.coords;
+            vm.map.center = {
+                lat: coords.latitude,
+                lng: coords.longitude,
+                zoom: 14
+            };
+            vm.map.markers = {
+                marker: {
+                    lat: coords.latitude,
+                    lng: coords.longitude
+                }
+            };
+        }
 
-    function initMapProperties() {
-      vm.map = {
-        defaults: {
-          zoomControl: false,
-          attributionControl: false,
-          doubleClickZoom: false,
-          scrollWheelZoom: false,
-          dragging: false,
-          touchZoom: false,
-        },
-        tiles: {
-          url: 'https://mt{s}.googleapis.com/vt?x={x}&y={y}&z={z}&style=high_dpi&w=512',
-          options: {
-            subdomains: [0, 1, 2, 3],
-            detectRetina: true,
-            tileSize: 512,
-            minZoom: 2,
-            maxZoom: 21,
-            reuseTiles: true,
-            noWrap: true
-          }
-        },
-        center: {
-          lat: 37.773204,
-          lng: -122.4213458,
-          zoom: 14
-        },
-        markers: {}
-      };
-    }
+        function onLocationError() {
+            $ionicPopup.alert({
+                title: 'Error',
+                template: 'Unable to retrieve your location'
+            });
+        }
 
-    function validZipCode() {
-      if (vm.zipcode && parseInt(vm.zipcode) > 0) {
-        $ionicLoading.show({
-          template: 'Checking...'
-        });
-        ZipcodesService.isValidZipcode(parseInt(vm.zipcode))
-          .then(function(isAvailable) {
-            $ionicLoading.hide();
-            if (isAvailable) {
-              LoginService.setUserZipCode(vm.zipcode);
-              $ionicHistory.nextViewOptions({
-                historyRoot: true
-              });
-              $state.go('app.buyer');
+        function initMapProperties() {
+            vm.map = {
+                defaults: {
+                    zoomControl: false,
+                    attributionControl: false,
+                    doubleClickZoom: false,
+                    scrollWheelZoom: false,
+                    dragging: false,
+                    touchZoom: false,
+                },
+                tiles: {
+                    url: 'https://mt{s}.googleapis.com/vt?x={x}&y={y}&z={z}&style=high_dpi&w=512',
+                    options: {
+                        subdomains: [0, 1, 2, 3],
+                        detectRetina: true,
+                        tileSize: 512,
+                        minZoom: 2,
+                        maxZoom: 21,
+                        reuseTiles: true,
+                        noWrap: true
+                    }
+                },
+                center: {
+                    lat: 37.773204,
+                    lng: -122.4213458,
+                    zoom: 14
+                },
+                markers: {}
+            };
+        }
+
+        function validZipCode() {
+            if (vm.zipcode && parseInt(vm.zipcode) > 0) {
+                $ionicLoading.show({
+                    template: 'Checking...'
+                });
+                ZipcodesService.isValidZipcode(parseInt(vm.zipcode))
+                    .then(function(isAvailable) {
+                        $ionicLoading.hide();
+                        if (isAvailable) {
+                            LoginService.setUserZipCode(vm.zipcode);
+                            $ionicHistory.nextViewOptions({
+                                historyRoot: true
+                            });
+                            $state.go('app.buyer');
+                        }
+                        else {
+                            $state.go('zipcode-unavailable', {
+                                zipcode: vm.zipcode
+                            });
+                        }
+                    });
             }
-            else {
-              $state.go('zipcode-unavailable', {
-                zipcode: vm.zipcode
-              });
-            }
-          });
-      }
-    }
+        }
 
-    function registerEmail() {
-      if (vm.email_address && $stateParams.zipcode) {
-        $ionicLoading.show({
-          template: 'Registering...'
-        });
-        //FAKE API CALL PASSING THE EMAIL ADDRESS AND THE ZIP CODE
-        $timeout(function() {
-          $ionicLoading.hide();
-          vm.email_address = null;
-          $ionicPopup.alert({
-            title: 'Thank you for registering',
-            template: 'Thank you, we will be in touch shortly.'
-          });
-        }, 1000);
-      }
+        function registerEmail() {
+            if (vm.email_address && $stateParams.zipcode) {
+                $ionicLoading.show({
+                    template: 'Registering...'
+                });
+                //FAKE API CALL PASSING THE EMAIL ADDRESS AND THE ZIP CODE
+                $timeout(function() {
+                    $ionicLoading.hide();
+                    vm.email_address = null;
+                    $ionicPopup.alert({
+                        title: 'Thank you for registering',
+                        template: 'Thank you, we will be in touch shortly.'
+                    });
+                }, 1000);
+            }
+        }
     }
-  }
 
 })();
--- app/scripts/zipcodes.service.js
+++ app/scripts/zipcodes.service.js
@@ -1,26 +1,26 @@
 'use strict';
 (function() {
-  angular.module('HomeCooked.services').factory('ZipcodesService', ZipcodesService);
+    angular.module('HomeCooked.services').factory('ZipcodesService', ZipcodesService);
 
-  ZipcodesService.$inject = ['$q', '$http', 'ENV', '_'];
-  function ZipcodesService($q, $http, ENV, _) {
-    var baseUrl = ENV.BASE_URL + '/api/v1/';
+    ZipcodesService.$inject = ['$q', '$http', 'ENV', '_'];
+    function ZipcodesService($q, $http, ENV, _) {
+        var baseUrl = ENV.BASE_URL + '/api/v1/';
 
-    return {
-      isValidZipcode: isValidZipcode
-    };
+        return {
+            isValidZipcode: isValidZipcode
+        };
 
-    function isValidZipcode(zipcode) {
-      var deferred = $q.defer();
-      var zipcodes = [];
-      $http.get(baseUrl + 'zipcodes/')
-        .success(function(resp) {
-          zipcodes = resp;
-        })
-        .finally(function() {
-          deferred.resolve(_.some(zipcodes, {zipcode: zipcode}));
-        });
-      return deferred.promise;
+        function isValidZipcode(zipcode) {
+            var deferred = $q.defer();
+            var zipcodes = [];
+            $http.get(baseUrl + 'zipcodes/')
+                .success(function(resp) {
+                    zipcodes = resp;
+                })
+                .finally(function() {
+                    deferred.resolve(_.some(zipcodes, {zipcode: zipcode}));
+                });
+            return deferred.promise;
+        }
     }
-  }
 })();
