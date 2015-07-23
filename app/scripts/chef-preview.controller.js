c78f86a1255b3158459fee707abd5dd3c35354f2
valdrinkoshi <valdrinkoshi@gmail.com>
2015-07-23 01:17:58 -0700

    handle login & payment before purchasing

--- app/scripts/chef-preview.controller.js
+++ app/scripts/chef-preview.controller.js
@@ -6,13 +6,17 @@
         .module('HomeCooked.controllers')
         .controller('ChefPreviewCtrl', ChefPreviewCtrl);
 
-    ChefPreviewCtrl.$inject = ['$state', '$stateParams', '$scope', '$ionicLoading', 'ChefService', 'LocationService', 'HCMessaging', '_'];
+    ChefPreviewCtrl.$inject = ['$state', '$stateParams', '$scope', '$ionicLoading', 'ChefService', 'LocationService', 'HCMessaging', 'LoginService', 'PaymentService', '_'];
 
-    function ChefPreviewCtrl($state, $stateParams, $scope, $ionicLoading, ChefService, LocationService, HCMessaging, _) {
+    function ChefPreviewCtrl($state, $stateParams, $scope, $ionicLoading, ChefService, LocationService, HCMessaging, LoginService, PaymentService, _) {
         var vm = this;
+        var user = LoginService.getUser();
 
         vm.go = $state.go;
-
+        vm.user = user;
+        vm.signin = signin;
+        vm.order = order;
+        vm.getNumber = getNumber;
         vm.chef = {};
 
         activate();
@@ -34,6 +38,27 @@
 
         function onLocationChange() {
             vm.chef.distance = LocationService.getDistanceFrom(vm.chef.location);
+        }
+
+        function order(qty) {
+            if (user.hasCC) {
+                PaymentService.order({dishId: $stateParams.dishId, quantity: qty});
+            }
+            else {
+                $state.go('app.settings-payment');
+            }
+        }
+
+        function getNumber(num) {
+            var a = [];
+            for (var i = 0; i < num; i++) {
+                a.push(i);
+            }
+            return a;
+        }
+
+        function signin() {
+            LoginService.login('facebook');
         }
 
     }
--- app/scripts/payment.service.js
+++ app/scripts/payment.service.js
@@ -1,18 +1,23 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular.module('HomeCooked.services').factory('PaymentService', PaymentService);
+    angular.module('HomeCooked.services').factory('PaymentService', PaymentService);
 
-  PaymentService.$inject = ['$http', 'ENV'];
-  function PaymentService($http, ENV) {
-    var baseUrl = ENV.BASE_URL + '/api/v1/';
+    PaymentService.$inject = ['$http', 'ENV'];
+    function PaymentService($http, ENV) {
+        var baseUrl = ENV.BASE_URL + '/api/v1/';
 
-    return {
-      savePaymentInfo: savePaymentInfo
-    };
+        return {
+            savePaymentInfo: savePaymentInfo,
+            order: order
+        };
 
-    function savePaymentInfo(info) {
-      return $http.post(baseUrl + 'payment-info/', info);
+        function savePaymentInfo(info) {
+            return $http.post(baseUrl + 'payment-info/', info);
+        }
+
+        function order(payload) {
+            return $http.post(baseUrl + ':dishId/order/', payload);
+        }
     }
-  }
 })();
