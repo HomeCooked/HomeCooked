bd77b88cdf145f2e0e0fdd7628c0b22b6d3e847c
valdrinkoshi <valdrinkoshi@gmail.com>
2015-07-29 01:23:45 -0700

    include facebook plugin. Enable both browser and device facebook login

--- app/scripts/login.service.js
+++ app/scripts/login.service.js
@@ -1,117 +1,127 @@
 'use strict';
 (function() {
-  angular.module('HomeCooked.services').factory('LoginService', LoginService);
+    angular.module('HomeCooked.services').factory('LoginService', LoginService);
 
-  LoginService.$inject = ['$q', '$http', 'ENV', 'CacheService', 'ChefService', '_'];
-  function LoginService($q, $http, ENV, CacheService, ChefService, _) {
-    var user = CacheService.getValue('user') || {};
+    LoginService.$inject = ['$q', '$http', 'ENV', 'CacheService', 'ChefService', '$cordovaFacebook', '_'];
+    function LoginService($q, $http, ENV, CacheService, ChefService, $cordovaFacebook, _) {
+        var user = CacheService.getValue('user') || {};
 
-    return {
-      login: login,
-      logout: logout,
-      getUser: getUser,
-      setUserZipCode: setUserZipCode,
-      becomeChef: becomeChef,
-      setIsChef: setIsChef
-    };
+        return {
+            login: login,
+            logout: logout,
+            getUser: getUser,
+            setUserZipCode: setUserZipCode,
+            becomeChef: becomeChef,
+            setIsChef: setIsChef
+        };
 
-    function setIsChef() {
-      if (user.isLoggedIn && !user.isChef) {
-        return ChefService.getChefInfo(user.id)
-          .then(function() {
-            user.isChef = true;
-            return true;
-          })
-          .catch(function() {
-            user.isChef = false;
-            return false;
-          });
-      }
-      return $q.when(user.isChef);
-    }
+        function setIsChef() {
+            if (user.isLoggedIn && !user.isChef) {
+                return ChefService.getChefInfo(user.id)
+                    .then(function() {
+                        user.isChef = true;
+                        return true;
+                    })
+                    .catch(function() {
+                        user.isChef = false;
+                        return false;
+                    });
+            }
+            return $q.when(user.isChef);
+        }
 
-    function login(type) {
-      return getAccessToken(type).then(function(accessToken) {
-        return homeCookedLogin(accessToken, type);
-      });
-    }
+        function login(type) {
+            return getAccessToken(type).then(function(accessToken) {
+                return homeCookedLogin(accessToken, type);
+            });
+        }
 
-    function logout() {
-      CacheService.invalidateCache();
-      _.forEach(user, function(val, key) {
-        user[key] = undefined;
-        delete user[key];
-      });
-    }
-
-    function getUser() {
-      return user;
-    }
-
-    function setUserZipCode(zipcode) {
-      user.zipcode = zipcode;
-      CacheService.setValue({user: user});
-    }
-
-    function becomeChef(chefInfo) {
-      chefInfo.user = user.id;
-      var deferred = $q.defer();
-      $http.post(ENV.BASE_URL + '/api/v1/enroll/', chefInfo)
-        .success(deferred.resolve)
-        .error(function(data) {
-          if (data.email && data.email[0] === 'This field must be unique.') {
-            deferred.resolve();
-          }
-          else {
-            deferred.reject(JSON.stringify(data));
-          }
-        });
-      return deferred.promise;
-    }
-
-    function homeCookedLogin(accessToken, provider) {
-      return $http.post(ENV.BASE_URL + '/connect/', {
-        'access_token': accessToken,
-        'client_id': ENV.CLIENT_ID,
-        'provider': provider
-      })
-        .then(function(response) {
-          var data = response.data;
-          // TODO this should come from the server
-          data.user.zipcode = user.zipcode;
-          _.assign(user, data.user, {isLoggedIn: true, isChef: false});
-          CacheService.setValue({
-            provider: provider,
-            credential: data.credential
-          });
-          return setIsChef();
-        })
-        .finally(function() {
-          if (user.isLoggedIn) {
-            CacheService.setValue({user: user});
-          }
-          else {
+        function logout() {
             CacheService.invalidateCache();
-          }
-        });
-    }
+            _.forEach(user, function(val, key) {
+                user[key] = undefined;
+                delete user[key];
+            });
+        }
 
-    function getAccessToken(provider) {
-      var deferred = $q.defer();
-      if (provider === 'facebook') {
-        window.openFB.login(function(response) {
-          if (response && response.authResponse && response.authResponse.token) {
-            deferred.resolve(response.authResponse.token);
-          }
-          else {
-            deferred.reject('not connected');
-          }
-        }, {scope: 'email'});
-      }
-      else {
-        deferred.reject('not supported');
-      }
-      return deferred.promise;
+        function getUser() {
+            return user;
+        }
+
+        function setUserZipCode(zipcode) {
+            user.zipcode = zipcode;
+            CacheService.setValue({user: user});
+        }
+
+        function becomeChef(chefInfo) {
+            chefInfo.user = user.id;
+            var deferred = $q.defer();
+            $http.post(ENV.BASE_URL + '/api/v1/enroll/', chefInfo)
+                .success(deferred.resolve)
+                .error(function(data) {
+                    if (data.email && data.email[0] === 'This field must be unique.') {
+                        deferred.resolve();
+                    }
+                    else {
+                        deferred.reject(JSON.stringify(data));
+                    }
+                });
+            return deferred.promise;
+        }
+
+        function homeCookedLogin(accessToken, provider) {
+            return $http.post(ENV.BASE_URL + '/connect/', {
+                'access_token': accessToken,
+                'client_id': ENV.CLIENT_ID,
+                'provider': provider
+            })
+                .then(function(response) {
+                    var data = response.data;
+                    // TODO this should come from the server
+                    data.user.zipcode = user.zipcode;
+                    _.assign(user, data.user, {isLoggedIn: true, isChef: false});
+                    CacheService.setValue({
+                        provider: provider,
+                        credential: data.credential
+                    });
+                    return setIsChef();
+                })
+                .finally(function() {
+                    if (user.isLoggedIn) {
+                        CacheService.setValue({user: user});
+                    }
+                    else {
+                        CacheService.invalidateCache();
+                    }
+                });
+        }
+
+        function getAccessToken(provider) {
+            if (provider === 'facebook') {
+                return doFacebookLogin()
+                    .then(function(response) {
+                        var token = _.get(response, 'authResponse.token') || _.get(response, 'authResponse.accessToken');
+                        if (token) {
+                            return token;
+                        }
+                        return $q.reject('not connected');
+                    });
+            }
+            return $q.reject('not supported');
+        }
+
+        function doFacebookLogin() {
+            var deferred = $q.defer();
+            var scope = ['public_profile', 'email'];
+            if (window.cordova) {
+                $cordovaFacebook.login(scope).then(deferred.resolve, deferred.reject);
+            }
+            else {
+                window.openFB.login(deferred.resolve, {scope: scope});
+            }
+            _.delay(deferred.reject.bind(deferred, 'timeout'), 60000);
+
+            return deferred.promise;
+        }
     }
-  }
 })();
