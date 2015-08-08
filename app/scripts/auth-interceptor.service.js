0c149edd72e683babdaaec28aaec7061cda439c0
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-07 19:19:38 -0700

    fix #86 - logout user on 401 unauthorized

--- app/scripts/auth-interceptor.service.js
+++ app/scripts/auth-interceptor.service.js
@@ -1,22 +1,22 @@
 'use strict';
 angular.module('HomeCooked.services')
-  .factory('AuthInterceptor', ['$q', 'CacheService',
-    function ($q, CacheService) {
-      return {
-        request: function (config) {
-          config.headers = config.headers || {};
-          var credential = CacheService.getValue('credential');
-          if (credential) {
-            config.headers.Authorization = credential.token_type + ' ' + credential.access_token;
-          }
-          return config;
-        },
-        response: function (response) {
-          if (response.status === 401) {
-            CacheService.invalidateCache();
-          }
-          return response || $q.when(response);
-        }
-      };
-    }]
+    .factory('AuthInterceptor', ['$q', 'CacheService', 'LoginService',
+        function($q, CacheService, LoginService) {
+            return {
+                request: function(config) {
+                    config.headers = config.headers || {};
+                    var credential = CacheService.getValue('credential');
+                    if (credential) {
+                        config.headers.Authorization = credential.token_type + ' ' + credential.access_token;
+                    }
+                    return config;
+                },
+                response: function(response) {
+                    if (response.status === 401) {
+                        LoginService.logout();
+                    }
+                    return response || $q.when(response);
+                }
+            };
+        }]
 );
