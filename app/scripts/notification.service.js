f863c7b30849d93a74b96855319dcf726ccfb0ad
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-09 22:08:03 -0700

    handle push notifications

--- app/scripts/notification.service.js
+++ app/scripts/notification.service.js
@@ -2,8 +2,8 @@
     'use strict';
     angular.module('HomeCooked.services').factory('NotificationService', NotificationService);
 
-    NotificationService.$inject = ['$rootScope', '$cordovaPush', '$http', 'CacheService', 'ENV', 'LoginService'];
-    function NotificationService($rootScope, $cordovaPush, $http, CacheService, ENV, LoginService) {
+    NotificationService.$inject = ['$rootScope', '$cordovaPush', '$cordovaDialogs', '$cordovaMedia', '$http', 'CacheService', 'ENV', 'LoginService'];
+    function NotificationService($rootScope, $cordovaPush, $cordovaDialogs, $cordovaMedia, $http, CacheService, ENV, LoginService) {
         var baseUrl = ENV.BASE_URL + '/api/v1/';
         var devices = {
             ios: {
@@ -11,23 +11,26 @@
                 config: {
                     'badge': true,
                     'sound': true,
-                    'alert': true
+                    'alert': true,
+                    'ecb': 'onNotificationAPN'
                 }
             },
             android: {
                 url: 'gcm',
                 config: {
-                    'badge': true,
-                    'sound': true,
-                    'alert': true
+                    'senderID': 'YOUR_GCM_PROJECT_ID' // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/43420598907
                 }
             }
         };
         var user = LoginService.getUser();
 
-        $rootScope.$watch(function() {
+        var loginWatcher = $rootScope.$watch(function() {
             return user.isLoggedIn;
         }, handleToken);
+
+
+        // Notification Received
+        $rootScope.$on('$cordovaPush:notificationReceived', handleNotification);
 
         return {
             register: register
@@ -40,18 +43,30 @@
             var device = getDevice();
             $cordovaPush.register(device.config)
                 .then(function(deviceToken) {
-                    device.token = deviceToken;
-                    handleToken();
+                    if (window.ionic.Platform.isIOS()) {
+                        device.token = deviceToken;
+                        handleToken();
+                    }
                 });
         }
 
         function handleToken() {
             var device = getDevice();
             if (user.isLoggedIn && device.token) {
+                loginWatcher();
                 // Success -- send deviceToken to server, and store for future use
                 $http.post(baseUrl + 'device/' + device.url, {deviceToken: device.token}).then(function() {
                     CacheService.setValue('device-token', device.token);
                 });
+            }
+        }
+
+        function handleNotification(event, notification) {
+            if (window.ionic.Platform.isAndroid()) {
+                handleAndroid(notification);
+            }
+            else if (window.ionic.Platform.isIOS()) {
+                handleIOS(notification);
             }
         }
 
@@ -62,6 +77,62 @@
 
         function getCurrentDeviceToken() {
             return CacheService.getValue('device-token');
+        }
+
+        // Android Notification Received Handler
+        function handleAndroid(notification) {
+            // ** NOTE: ** You could add code for when app is in foreground or not, or coming from coldstart here too
+            //             via the console fields as shown.
+            console.log('In foreground ' + notification.foreground + ' Coldstart ' + notification.coldstart);
+            if (notification.event === 'registered') {
+                devices.android.token = notification.regid;
+                handleToken();
+            }
+            else if (notification.event === 'message') {
+                $cordovaDialogs.alert(notification.message, 'Push Notification Received');
+            }
+            else if (notification.event === 'error') {
+                $cordovaDialogs.alert(notification.msg, 'Push notification error event');
+            }
+            else {
+                $cordovaDialogs.alert(notification.event, 'Push notification handler - Unprocessed Event');
+            }
+        }
+
+        // IOS Notification Received Handler
+        function handleIOS(notification) {
+            // The app was already open but we'll still show the alert and sound the tone received this way. If you didn't check
+            // for foreground here it would make a sound twice, once when received in background and upon opening it from clicking
+            // the notification when this code runs (weird).
+            if (notification.foreground === '1') {
+                // Play custom audio if a sound specified.
+                if (notification.sound) {
+                    var mediaSrc = $cordovaMedia.newMedia(notification.sound);
+                    mediaSrc.promise.then($cordovaMedia.play(mediaSrc.media));
+                }
+
+                if (notification.body && notification.messageFrom) {
+                    $cordovaDialogs.alert(notification.body, notification.messageFrom);
+                }
+                else {
+                    $cordovaDialogs.alert(notification.alert, 'Push Notification Received');
+                }
+
+                if (notification.badge) {
+                    $cordovaPush.setBadgeNumber(notification.badge);
+                }
+            }
+            // Otherwise it was received in the background and reopened from the push notification. Badge is automatically cleared
+            // in this case. You probably wouldn't be displaying anything at this point, this is here to show that you can process
+            // the data in this situation.
+            else {
+                if (notification.body && notification.messageFrom) {
+                    $cordovaDialogs.alert(notification.body, '(RECEIVED WHEN APP IN BACKGROUND) ' + notification.messageFrom);
+                }
+                else {
+                    $cordovaDialogs.alert(notification.alert, '(RECEIVED WHEN APP IN BACKGROUND) Push Notification Received');
+                }
+            }
         }
     }
 })();
