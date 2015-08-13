8ac5f23513853766c702adcf2bbdb0f7d9710503
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-13 02:23:38 -0700

    update to 0.0.5 (icons for android too)

---
+++ build-android.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ionic prepare android
+cordova build --release android
+echo "Copying to build/"
+rm build/android-release-unsigned.apk
+cp platforms/android/build/outputs/apk/android-release-unsigned.apk build/
+cd build/
+echo "Signing apk"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
+FINAL_APK="HomeCooked-$1.apk"
+rm $FINAL_APK
+~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-release-unsigned.apk $FINAL_APK
+echo "Deleting temp files"
+rm android-release-unsigned.apk
+cd ..
+echo "Done!"
