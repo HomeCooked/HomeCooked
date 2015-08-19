7cea83cc7ef63975409ec99b5e4a2fd319478a83
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-19 00:48:19 -0700

    have the keyboard bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -176,11 +176,7 @@ HomeCooked
                     appId: ENV.FACEBOOK_APP_ID
                 });
             }
-            // Hide the accessory b ar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
+
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
