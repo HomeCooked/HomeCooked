fc55dc09c251804de1645888b82a5b9f5bc6ed10
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-19 00:49:17 -0700

    disable native scrolling to avoid funky animations when keyboard appears

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -177,6 +177,9 @@ HomeCooked
                 });
             }
 
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
