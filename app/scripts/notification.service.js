1dc80af37e24acc50e481ad563a0286bc1d5d14a
valdrinkoshi <valdrinkoshi@gmail.com>
2015-08-21 18:06:05 -0700

    handle redirect param for notification

--- app/scripts/notification.service.js
+++ app/scripts/notification.service.js
@@ -2,8 +2,8 @@
     'use strict';
     angular.module('HomeCooked.services').factory('NotificationService', NotificationService);
 
-    NotificationService.$inject = ['$log', '$rootScope', '$cordovaPush', '$cordovaDialogs', '$http', 'CacheService', 'ENV', 'LoginService', 'HCMessaging', '_'];
-    function NotificationService($log, $rootScope, $cordovaPush, $cordovaDialogs, $http, CacheService, ENV, LoginService, HCMessaging, _) {
+    NotificationService.$inject = ['$log', '$rootScope', '$cordovaPush', '$state', '$ionicHistory', '$http', 'CacheService', 'ENV', 'LoginService', 'HCMessaging', '_'];
+    function NotificationService($log, $rootScope, $cordovaPush, $state, $ionicHistory, $http, CacheService, ENV, LoginService, HCMessaging, _) {
         var devices = {
             ios: {
                 url: ENV.BASE_URL + '/api/v1/device/apns/',
@@ -98,13 +98,16 @@
                 handleToken();
             }
             else if (notification.event === 'message') {
-                $cordovaDialogs.alert(notification.message, 'Push Notification Received');
+                if (notification.redirect) {
+                    go(notification.redirect);
+                }
+                HCMessaging.showMessage('', notification.message);
             }
             else if (notification.event === 'error') {
-                $cordovaDialogs.alert(notification.msg, 'Push notification error event');
+                HCMessaging.showError(notification.msg);
             }
             else {
-                $cordovaDialogs.alert(notification.event, 'Push notification handler - Unprocessed Event');
+                HCMessaging.showMessage('', notification.event);
             }
         }
 
@@ -113,32 +116,28 @@
             // The app was already open but we'll still show the alert and sound the tone received this way. If you didn't check
             // for foreground here it would make a sound twice, once when received in background and upon opening it from clicking
             // the notification when this code runs (weird).
+            if (notification.redirect) {
+                go(notification.redirect);
+            }
             if (notification.foreground === '1') {
                 // Play custom audio if a sound specified.
                 //TODO
-
-                if (notification.body && notification.messageFrom) {
-                    $cordovaDialogs.alert(notification.body, notification.messageFrom);
-                }
-                else {
-                    $cordovaDialogs.alert(notification.alert, 'Push Notification Received');
+                if (notification.message) {
+                    HCMessaging.showMessage('', notification.message);
                 }
 
                 if (notification.badge) {
                     $cordovaPush.setBadgeNumber(notification.badge);
-                }
+                }g
             }
-            // Otherwise it was received in the background and reopened from the push notification. Badge is automatically cleared
-            // in this case. You probably wouldn't be displaying anything at this point, this is here to show that you can process
-            // the data in this situation.
-            else {
-                if (notification.body && notification.messageFrom) {
-                    $cordovaDialogs.alert(notification.body, '(RECEIVED WHEN APP IN BACKGROUND) ' + notification.messageFrom);
-                }
-                else {
-                    $cordovaDialogs.alert(notification.alert, '(RECEIVED WHEN APP IN BACKGROUND) Push Notification Received');
-                }
-            }
+        }
+
+        function go(state) {
+            $ionicHistory.nextViewOptions({
+                historyRoot: true,
+                disableAnimate: true
+            });
+            $state.go(state);
         }
     }
 })();
--- build-android.sh
+++ build-android.sh
@@ -6,7 +6,7 @@ rm build/android-release-unsigned.apk
 cp platforms/android/build/outputs/apk/android-release-unsigned.apk build/
 cd build/
 echo "Signing apk"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore -storepass h0mec00ked -keypass Nirdlav0 android-release-unsigned.apk alias_name
 FINAL_APK="HomeCooked-$1.apk"
 rm $FINAL_APK
 ~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-release-unsigned.apk $FINAL_APK
