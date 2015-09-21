34ecd82474e1ff447bb7814f265f3f3d63691e72
valdrinkoshi <valdrinkoshi@gmail.com>
2015-09-21 01:42:38 -0700

    enable native scroll and accessory bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -171,7 +171,8 @@ HomeCooked
             }
 
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.disableScroll(false);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
