ecb4170dd413ee47fc65bd0cdd628b0a8d008db4
valdrinkoshi <valdrinkoshi@gmail.com>
2016-01-06 00:55:11 +0000

    move to gulp

---
+++ www/scripts/config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+ angular.module("config", [])
+
+.constant("ENV", {
+  "name": "production",
+  "version": "0.3.8",
+  "BASE_URL": "https://homecooked.herokuapp.com",
+  "FACEBOOK_APP_ID": "805673482820123"
+})
+
+;
