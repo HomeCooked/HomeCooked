753e451dc01de405cd2a4b1987580507e8eac952
valdrinkoshi <valdrinkoshi@gmail.com>
2016-01-06 00:04:49 +0000

    move scripts on head, update ionic to 1.2.4

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,9 @@ var paths = {
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./app/styles/main.scss')
     .pipe(sass())
+    .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
